4a4053d44f1f0d6e193bec0ccbe84d7159cfedc6
jenlainacoyle <jenlainacoyle@users.noreply.github.com>
2015-03-31 21:02:25 -0400

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -27,6 +27,6 @@ featureLayer.on('ready',function(){
 
 featureLayer.on('ready', function(){
   this.eachLayer(function(layer){
-    layer.bindPopup('Hi, Im the bike route called ' + layer.feature.properties.name);
+    layer.bindPopup('Hi, Im the bike route called ' + layer.feature.properties.foot);
 });
 });
