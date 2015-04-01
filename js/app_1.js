843ca36307655a0e79068462eb28b8ef06bea745
jenlainacoyle <jenlainacoyle@users.noreply.github.com>
2015-03-31 20:31:40 -0400

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -8,6 +8,19 @@ var token = 'pk.eyJ1IjoiamVubGFpbmFjb3lsZSIsImEiOiI0ZEs2Y2lZIn0.bI5bfj0mTgm79wsg
 L.mapbox.accessToken = token;
 var map = L.mapbox.map('map', mapId);
 
-
 //Set the view of the map to the whole US
 map.setView([39, -96], 4);
+
+var dataFileToAdd = 'data/parks.geojson';
+
+var featureLayer = L.mapbox.featureLayer().loadURL(dataFileToAdd).addTo(map)
+
+featureLayer.on('ready',function(){
+  this.setStyle({
+    'color':'#eco008',
+    'fillColor':'#ec008c',
+    'weight':4,
+    'opacity':.7
+  });
+  map.fitBounds(featureLayer.getBounds());
+});
