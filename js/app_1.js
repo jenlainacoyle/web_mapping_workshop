afeb46c37f4fba29bb1ef44fc4ffa1f6ee337e65
jenlainacoyle <jenlainacoyle@users.noreply.github.com>
2015-03-31 20:36:38 -0400

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -13,7 +13,7 @@ map.setView([39, -96], 4);
 
 var dataFileToAdd = 'data/dc_national_parks.geojson';
 
-var featureLayer = L.mapbox.featureLayer().loadURL(dataFileToAdd).addTo(map)
+var featureLayer = L.mapbox.featureLayer().loadURL(dataFileToAdd).addTo(map);
 
 featureLayer.on('ready',function(){
   this.setStyle({
